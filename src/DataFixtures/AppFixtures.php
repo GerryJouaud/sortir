a078fceab8bb2353daaac1d3ac9e17fc456dbbdf
aahmedabdelrahm2024 <abdelrahman.ahmedabdelrahmane2024@campus-eni.fr>
2024-06-25 09:48:03 +0200

    create fixture and faker v02

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,8 +6,10 @@ namespace App\DataFixtures;
 
 use App\Entity\Campus;
 use App\Entity\City;
+use App\Entity\Event;
 use App\Entity\Place;
 use App\Entity\StateEvent;
+use Couchbase\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
@@ -17,6 +19,12 @@ use Faker\Generator;
 class AppFixtures extends Fixture{
 
     private readonly Generator $faker;
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
 
     public function load(ObjectManager $manager): void
     {
@@ -28,7 +36,31 @@ class AppFixtures extends Fixture{
         $this->addUser($manager, $faker);
         $this->addEvent($manager, $faker);
 
-       $manager->flush();
+        $manager->flush();
+    }
+    public function addUser(ObjectManager $manager, Generator $generator)
+    {
+        $campus = $manager->getRepository(Campus::class)->findAll();
+
+        for ($i = 0; $i < 50; $i++) {
+            $user = new User();
+            $user
+                ->setUserName($generator->userName)
+                ->setRoles(["ROLE_USER"]);
+            $password = $this->passwordHasher->hashPassword($user,'1234');
+            $user
+                ->setPassword($password)
+                ->setLastName($generator->lastName)
+                ->setFirstName($generator->firstName)
+                ->setPhone($generator->phoneNumber)
+                ->setEmail($generator->email)
+                ->setState($generator->boolean())
+                ->setCampus($generator->randomElement($campus));
+            $manager->persist($user);
+        }
+
+        $manager->flush();
+
     }
 
     public function addCampus(ObjectManager $manager)
@@ -90,10 +122,43 @@ class AppFixtures extends Fixture{
         $campus = $manager->getRepository(Campus::class)->findAll();
         $place = $manager->getRepository(Place::class)->findAll();
         $stateEvent = $manager->getRepository(StateEvent::class)->findAll();
+        $organizer = $manager ->getRepository(User::class)->findAll();
 
+        for($i = 0; $i < 10; $i++){
+            $event = new Event();
+            $event
+                ->setName($generator->word)
+                ->setStartDate($generator->dateTimeBetween('-1 month', '+6 month'))
+                ->setDuration($generator->numberBetween(30,240))
+                ->setDateLine($generator->dateTimeBetween("-1 month", " +6 month"))
+                ->setMaxParticipants($generator->numberBetween(5,50))
+                ->setDescription($generator->text)
+                ->setCampus($generator->randomElement($campus))
+                ->setPlace($generator->randomElement($place))
+                ->setStateEvent($generator->randomElement($stateEvent))
+                ->setOrganizer($organizer->randomElement($organizer));
+            $manager->persist($event);
+        }
 
+        $manager->flush();
     }
 
+    public  function addPlace(ObjectManager $manager){
+        $cities = $manager ->getRepository(City::class)->findAll();
+        for ($i = 0 ; $i<10; $i++){
+            $place = new Place();
+            $place
+                ->setName($generator->word)
+                ->setStreet($generator->streetAddress)
+                ->setLatitude($generator->latitude)
+                ->setLongitude($generator->longitude)
+                ->setCity($generator->randomElement($cities));
+            $manager->persist($place);
+
+        }
+
+        $manager->flush();
+    }
     public function addStateEvent(ObjectManager $manager)
     {
         $stateEventCreated = new State();
