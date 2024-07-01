33c4c6bce41729e5d5eea7d9ddf8e0f5ce9712d4
gjouaud2024 <gerry.jouaud@gmail.com>
2024-07-01 10:09:46 +0200

    Version propre 01/07/2024

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,20 +2,49 @@
 
 namespace App\Controller;
 
+use App\Repository\CampusRepository;
 use App\Repository\EventRepository;
+use App\Repository\PlaceRepository;
+use App\Repository\StateEventRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class MainController extends AbstractController
 {
-    #[Route('/', name: 'app_main')]
-    public function index(EventRepository $eventRepository): Response
-    {
-        $events = $eventRepository->findAll();
+    #[Route('/', name: 'list')]
+    public function list(
+        EntityManagerInterface $entityManager,
+        EventRepository $eventRepository,
+        PlaceRepository $placeRepository,
+        StateEventRepository $stateEventRepository,
+        Request $request,
+        CampusRepository $campusRepository
 
+    ): Response
+    {
+
+        $allCampus=$campusRepository->findAll();
+
+        $filters = [
+            'campus' => $request->query->get('campus'),
+            'search' => $request->query->get('search'),
+            'startDate' => $request->query->get('start_date'),
+            'dateLine' => $request->query->get('dateLine'),
+            'organisateur' => $request->query->get('organisateur'),
+            'inscrit' => $request->query->get('inscrit'),
+            'non_inscrit' => $request->query->get('non_inscrit'),
+            'passees' => $request->query->get('passees'),
+        ];
+        $events = $eventRepository->findByFilters($filters, $this->getUser());
+
+        //Récupération d'un event par son id
         return $this->render('main/index.html.twig', [
             'events' => $events,
+            'allCampus' => $allCampus,
+            'filters' => $filters,
         ]);
     }
 }
